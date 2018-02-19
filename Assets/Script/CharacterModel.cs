66c740f2a90bf6781276dad45027fbdc8d7fc552
Konatsup <konatsukomatsu@gmail.com>
2018-02-19 17:12:09 +0900

    initial commit

---
+++ Assets/Script/CharacterModel.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterModel {
+	int HP; //HP
+	int AT; //攻撃力
+	int MP; //MP
+	int DE; //防御力
+	string serihu;
+	string name;
+	string jiseinoku;
+	bool sleepFlg;
+	bool mahiFlg;
+	bool poizonFlg;
+	bool[] status;
+	int refCount;
+	bool refectFlg;
+
+	public CharacterModel(){
+
+	}
+
+
+}
---
+++ Assets/Script/GameManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour {
+
+	public Text log;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	//純粋なセッター
+	void SetLogClean(string txt){
+		log.text = txt + "\n";
+	}
+
+	//ログに反映させるメソッド
+	void SetLogAdd(string txt){
+		log.text = log.text + txt + "\n";
+	}
+
+
+}
